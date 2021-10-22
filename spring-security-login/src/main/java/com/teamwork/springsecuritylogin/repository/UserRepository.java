c22d0fee3731eedda32d2b999b953e979c818ead
Phan Thanh Long <longphanthanh@tech.admicro.vn>
2021-10-22 11:22:14 +0700

    init new

--- spring-security-login/src/main/java/com/teamwork/springsecuritylogin/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teamwork.springsecuritylogin.repository;
-
-import com.teamwork.springsecuritylogin.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
-}
